09009fefa314e5d94a208f308d34b5cae27b8260
Дмитрий <white-wolfs@mail.ru>
2022-09-18 10:37:33 +0300

    task-2-v1.1

--- Module 26/Task-2/main.cpp
+++ Module 26/Task-2/main.cpp
@@ -3,121 +3,144 @@
 
 class PhoneBook
 {
-std::vector <std::vector<std::string>> book;
-std::string lastName;
-std::string numberPhone;
+    std::vector <std::vector<std::string>> book;
+    std::string lastName;
+    std::string numberPhone;
 
 public:
-  std::string checkNumber (std::string number)
-  {
-    std::cout << "Input number phone contacts:" << std::endl;
-    std::cin >> number;
 
-      for (int i = 0; i < 2; ++i)
-      {
-        if (number[0] != '+' || number[1] != '7') {
-          std::cerr << "Error input numbers!" << std::endl;
-          std::cout << "Input number phone contacts:" << std::endl;
-          std::cin >> number;
-        }
-      }
-      for (int k = 2; k < 12; ++k)
-      {
-        if (number[k] < '0' || number[k] > '9' || number.size() != 12)
-        {
-          std::cerr << "Error input numbers!" << std::endl;
-          std::cout << "Input number phone contacts:" << std::endl;
-          std::cin >> number;
-        }
-      }
-    return number;
-  }
+    static std::string checkNumber (std::string number)
+    {
+        std::cout << "Input number phone contacts:" << std::endl;
+        std::cin >> number;
 
-  void showContacts ()
-  {
-    for (int h = 0; h < book.size(); h++){
-      for ( int k = 0; k < book[h].size(); k++){
-        if (k == 0)
+        for (int i = 0; i < 2; ++i)
         {
-          std::cout << "Name Contacts: "<< book[h][k] << std:: endl;
+            if (number[0] != '+' || number[1] != '7') {
+                std::cerr << "Error input numbers!" << std::endl;
+                std::cout << "Input number phone contacts:" << std::endl;
+                std::cin >> number;
+            }
         }
-        else if (k == 1)
+        for (int k = 2; k < 12; ++k)
         {
-          std::cout << "Number Contacts: " << book[h][k] << std:: endl;
+            if (number[k] < '0' || number[k] > '9' || number.size() != 12)
+            {
+                std::cerr << "Error input numbers!" << std::endl;
+                std::cout << "Input number phone contacts:" << std::endl;
+                std::cin >> number;
+            }
         }
-
-      }
-      std::cout << std::endl;
+        return number;
     }
-  }
 
-  void contacts()
-  {
-    std::cout << "Input Last Name contacts:" << std::endl;
-    std::cin >> lastName;
-    numberPhone = checkNumber(numberPhone);
-    book.push_back(std::vector<std::string>{lastName, numberPhone});
+    void showContacts (int h)
+    {
+        //for (int h = 0; h < book.size(); h++){
+        for ( int k = 0; k < book[h].size(); k++){
+            if (k == 0)
+            {
+                std::cout << "Name Contacts: "<< book[h][k] << std:: endl;
+            }
+            else if (k == 1)
+            {
+                std::cout << "Number Contacts: " << book[h][k] << std:: endl;
+            }
 
-    showContacts();
-  }
+        }
+        std::cout << std::endl;
+        // }
+    }
 
+    void contacts()
+    {
+        std::cout << "Input Last Name contacts:" << std::endl;
+        std::cin >> lastName;
+        numberPhone = checkNumber(numberPhone);
+        book.push_back(std::vector<std::string>{lastName, numberPhone});
 
-};
-
-class SearchContacts
-{
-
+    }
+    void search(const std::string& requestContacts, const std::string& operation)
+    {
+        if (!book.empty()){
+            for (int h = 0; h < book.size() ; h++){
+                for ( int k = 0; k < book[h].size(); k++){
+                    if ((k == 0 && book[h][k] == requestContacts) ||
+                        (k == 1 && book[h][k] == requestContacts))
+                    {
+                        if (operation == "call") {
+                            std::cout << "Contact called:" << std::endl;
+                            showContacts(h);
+                            break;
+                        }
+                        else if (operation == "sms"){
+                            std::cout << "The message was sent to the contact:" << std::endl;
+                            showContacts(h);
+                            break;
+                        }
+                    } else if (k == book.size())
+                        std::cout << "Contact not found!" << std::endl;
+                }
+            }
+        }
+        else
+            std::cout << "Contact not found!" << std::endl;
+    }
 };
 
 class Phone
 {
-PhoneBook* phoneBook = new PhoneBook();
-SearchContacts* searchContacts = new SearchContacts();
-std::string searchNumber;
-std::string searchName;
+    PhoneBook* phoneBook = new PhoneBook();
+    std::string request;
+    std::string operation;
 
 public:
-  void add ()
-  {
-    phoneBook -> contacts();
-  }
-  void call ()
-  {
+    void add ()
+    {
+        phoneBook -> contacts();
+    }
+    void call ()
+    {
 
-  }
-  void sms ()
-  {
-
-  }
+        std::cout << "Enter the contact's number or name:" << std::endl;
+        std::cin >> request;
+        phoneBook -> search(request, operation = "call");
+    }
+    void sms ()
+    {
+        std::cout << "Enter the contact's number or name:" << std::endl;
+        std::cin >> request;
+        phoneBook -> search(request, operation = "sms");
+    }
 
 };
 
 int main() {
-  Phone* phone = new Phone();
-  std::string command;
+    auto* phone = new Phone();
+    std::string command;
 
-  while (command != "exit"){
+    while (command != "exit"){
 
-    std::cout << "Input command phone: (add/call/sms/exit)" << std:: endl;
-    std::cin >> command;
+        std::cout << "Input command phone: (add/call/sms/exit)" << std:: endl;
+        std::cin >> command;
 
-    if (command == "add")
-    {
-      phone -> add();
+        if (command == "add")
+        {
+            phone -> add();
+        }
+        else if (command == "call")
+        {
+            phone -> call();
+        }
+        else if (command == "sms")
+        {
+            phone -> sms();
+        }
+        else if (command != "exit")
+        {
+            std::cerr << "Error input!" << std:: endl;
+        }
     }
-    else if (command == "call")
-    {
-      phone -> call();
-    }
-    else if (command == "sms")
-    {
-      phone -> sms();
-    }
-    else if (command != "exit")
-    {
-      std::cerr << "Error input!" << std:: endl;
-    }
-  }
-  std::cout << "The program is closed!" << std::endl;
-  return 0;
+    std::cout << "The program is closed!" << std::endl;
+    return 0;
 }
