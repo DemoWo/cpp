9eb13b14b8e582639c4934c9c271f642a034b086
Demon <v1entro@ya.ru>
2022-11-27 10:58:38 +0300

    villageElf-cont1

--- Module 27/VillageElf/main.cpp
+++ Module 27/VillageElf/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <vector>
+
+class Village
+        {
+    std::vector <int> branch;
+
+        public:
+
+    struct BRANCH
+    {
+        std::string *nameElf;
+        std::vector <int> bigBranch;
+        std::vector <int> middleBranch;
+    };
+
+            static void elfHome ()
+            {
+                BRANCH branch1;
+
+              for (int i = 0; i < 5; i++)
+              {
+                  branch1.bigBranch.push_back(3 + rand() % (6 - 3));
+
+                  for (int k = 0; k < branch1.bigBranch[i]; k++)
+                    branch1.middleBranch.push_back(2 + rand() % (4 - 2));
+              }
+              for (int j = 0; j < 5; j++)
+              {
+                  std::cout << branch1.bigBranch[j] << " / " ;
+
+                  for (int h = 0; h < branch1.bigBranch[j]; h++)
+                    std:: cout << branch1.middleBranch[j] << " ";
+                  std::cout << std::endl;
+              }
+            }
+        };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Village::elfHome();
     return 0;
 }
