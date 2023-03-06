cfc60a7b77a606537ff008bda7593bc80dbfdb52
Demon <v1entro@ya.ru>
2023-03-06 09:35:55 +0300

    06/03/23 task-3

---
+++ Module 28/SimulationOfTheStationOperation/cmake-build-debug-mingw/CMakeFiles/SimulationOfTheStationOperation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimulationOfTheStationOperation.dir/main.cpp.obj"
+  "CMakeFiles/SimulationOfTheStationOperation.dir/main.cpp.obj.d"
+  "SimulationOfTheStationOperation.exe"
+  "SimulationOfTheStationOperation.exe.manifest"
+  "SimulationOfTheStationOperation.pdb"
+  "libSimulationOfTheStationOperation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimulationOfTheStationOperation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
