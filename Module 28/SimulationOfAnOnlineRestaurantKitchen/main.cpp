532b5c03e6991a1d037b283f8b5a477a71227c83
Дмитрий <white-wolfs@mail.ru>
2023-03-06 22:06:51 +0300

    06/03/23 task-3-v1

--- Module 28/SimulationOfAnOnlineRestaurantKitchen/main.cpp
+++ Module 28/SimulationOfAnOnlineRestaurantKitchen/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <vector>
+
+int genRandom(int min, int max) {
+    static thread_local std::random_device rd;
+    std::uniform_int_distribution<int> uid(min, max);
+    return uid(rd);
+}
 
 enum MENU
         {
@@ -9,6 +19,14 @@ enum MENU
     salad,
     sushi,
         };
+
+std::queue <int> orders;
+std::queue<int> cooked;
+std::mutex work_lock;
+std::mutex console_access;
+int count = 0;
+int max = 10;
+bool cooking = false;
 
 std::string menuRestaurant  (int cmdMenu)
 {
@@ -24,22 +42,63 @@ std::string menuRestaurant  (int cmdMenu)
             return  "Salad";
         case MENU::sushi:
             return "Sushi";
+        default:
+            return "unk";
     }
-    return nullptr;
+}
+
+void orderDelivery () //доставка заказа
+{
+    while (count < max) {
+        std::this_thread::sleep_for(std::chrono::seconds(30));
+        std::lock_guard<std::mutex> lock{ console_access };
+        if (cooked.empty())
+            continue;
+        auto first = cooked.front();
+        std::cout << "Courier picked up " << menuRestaurant(first) << std::endl;
+        cooked.pop();
+        count++;
+    }
+}
+
+void orderPreparation ()  // приготовление заказа
+{
+    while (count < max) {
+        int cookingTime = genRandom(5, 15);
+        std::this_thread::sleep_for(std::chrono::seconds(cookingTime));
+        std::lock_guard<std::mutex> lock{ work_lock };
+        if (orders.empty() || cooking) continue;
+        cooking = true;
+        auto first = orders.front();
+        console_access.unlock();
+        std::cout << "Order preparation: " << menuRestaurant(first) << "! Through " << cookingTime << " seconds!"
+                  << std::endl;
+        console_access.lock();
+        cooked.push(first);
+        orders.pop();
+        cooking = false;
+    }
+}
+
+void order () // заказ
+{
+    while (count < max) {
+        std::this_thread::sleep_for(std::chrono::seconds(genRandom(5, 10)));
+        std::lock_guard<std::mutex> lock{work_lock};
+        int numOrder = genRandom(1, 5);
+        console_access.unlock();
+        std::cout << "The order is expected to arrive!" << std::endl;
+        console_access.lock();
+        orders.push(numOrder);
+    }
 }
 
 int main() {
-
-    int i;
-    int count = 0;
-while (count != 10 )
-{
-    i = std::rand() % 5 +1;
-    std::cout << menuRestaurant(i) << std::endl;
-    count ++;
-}
-
-
-
+    std::thread expectation (order);
+    std::thread preparation (orderPreparation);
+    std::thread delivery (orderDelivery);
+    expectation.join();
+    preparation.join();
+    delivery.join();
     return 0;
 }
