f89f0f9465ad8f22b912d14f39eeabdf6f3a93fa
Demon <v1entro@ya.ru>
2023-03-16 15:13:57 +0300

    Task-2-m30-v2

--- Module 30/CapturingTheTitleOfAWebPage/main.cpp
+++ Module 30/CapturingTheTitleOfAWebPage/main.cpp
@@ -2,7 +2,9 @@
 #include <cpr/cpr.h>
 
 int main() {
-    cpr::Response r = cpr::Get(cpr::Url("https://httpbin.org/get"));
-    std::cout << r.text << std::endl;
+    cpr::Response r = cpr::Get(cpr::Url("https://httpbin.org/html"));
+    std::size_t b = r.text.find("<h1>"), e = r.text.find("</h1>");
+    std::string header = r.text.substr(b + 4, e - b - 4);
+    std::cout << header << std::endl;
     return 0;
 }
