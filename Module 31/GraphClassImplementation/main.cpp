bbfb813022c2957c380112c4890de941d3e87919
Demon <v1entro@ya.ru>
2023-03-21 21:09:07 +0300

    M31-Task1&2

--- Module 31/GraphClassImplementation/main.cpp
+++ Module 31/GraphClassImplementation/main.cpp
@@ -1,18 +1,192 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
 
 class IGraph {
 public:
     virtual ~IGraph() {}
     IGraph() {};
-    IGraph(IGraph *_oth) {};
+    IGraph(const IGraph& _oth) {};
     virtual void AddEdge(int from, int to) = 0; // Метод принимает вершины начала и конца ребра и добавляет ребро
     virtual int VerticesCount() const = 0; // Метод должен считать текущее количество вершин
-    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const = 0; // Для конкретной вершины метод выводит в вектор “вершины” все вершины, в которые можно дойти по ребру из данной
-    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const = 0; // Для конкретной вершины метод выводит в вектор “вершины” все вершины, из которых можно дойти по ребру в данную
+    virtual void GetNextVertices(int vertex, std::vector<int>& vertices) const = 0; // Для конкретной вершины метод выводит в вектор “вершины” все вершины, в которые можно дойти по ребру из данной
+    virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const = 0; // Для конкретной вершины метод выводит в вектор “вершины” все вершины, из которых можно дойти по ребру в данную
+    virtual std::vector<int> GetVertices() const = 0; // Вернуть все вершины. Нужно для копирования.
 };
 
-int main() {
+class ListGraph final : public IGraph {
+    std::vector<std::vector<int>> peeks;
 
-    return 0;
+    int FindEdge(int v) const {
+        for (int i = 0, ie = VerticesCount(); i != ie; ++i) {
+            if (peeks[i][0] == v)
+                return i;
+        }
+        return -1;
+    }
+public:
+    ListGraph() {}
+
+    ListGraph(const IGraph& _oth) {
+        peeks.clear();
+        std::vector<int> vert = _oth.GetVertices();
+        std::vector<int> tmp;
+        for (auto& from : vert) {
+            _oth.GetNextVertices(from, tmp);
+            for (auto& to : tmp) {
+                AddEdge(from, to);
+            }
+            tmp.clear();
+        }
+    }
+
+    virtual void AddEdge(int from, int to) override {
+        int v = FindEdge(from);
+        int t = FindEdge(to);
+        if (v != -1) {
+            if (std::find(peeks[v].begin(), peeks[v].end(), v) == peeks[v].end())
+                peeks[v].push_back(to);
+        }
+        else {
+            peeks.push_back({ from, to });
+        }
+    }
+
+    virtual int VerticesCount() const override {
+        return static_cast<int>(peeks.size());
+    }
+
+    virtual void GetNextVertices(int vertex, std::vector<int>& vertices) const override {
+        int v = FindEdge(vertex);
+        if (v != -1) {
+            std::vector<int> tmp(peeks[v].begin() + 1, peeks[v].end());
+            vertices = tmp;
+        }
+    }
+
+    virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const override {
+        for (int i = 0, ie = VerticesCount(); i != ie; ++i) {
+            for (int j = 1, je = static_cast<int>(peeks[i].size()); j != je; ++j) {
+                if (peeks[i][j] == vertex)
+                    vertices.push_back(peeks[i][j]);
+            }
+        }
+    }
+
+    virtual std::vector<int> GetVertices() const override {
+        std::vector<int> tmp;
+        for (auto& p : peeks) {
+            tmp.push_back(p[0]);
+        }
+        return tmp;
+    }
+};
+
+class MatrixGraph final : public IGraph {
+    std::vector<std::vector<int>> matrix;
+
+public:
+    MatrixGraph() {};
+    MatrixGraph(const IGraph& _oth) {
+        matrix.clear();
+        std::vector<int> vert = _oth.GetVertices();
+        std::vector<int> tmp;
+        for (auto& from : vert) {
+            _oth.GetNextVertices(from, tmp);
+            for (auto& to : tmp) {
+                AddEdge(from, to);
+            }
+            tmp.clear();
+        }
+    };
+    virtual void AddEdge(int from, int to) override {
+        int max = (from > to ? from : to) + 1;
+        if (matrix.size() < max) {
+            matrix.resize(max);
+            for (auto& e : matrix) {
+                if (e.size() < max)
+                    e.resize(max, 0);
+            }
+            for (int i = 0, ie = max; i < max; ++i) {
+                matrix[i][0] = i;
+                matrix[0][i] = i;
+            }
+        }
+        matrix[from][to] = 1;
+    }
+    virtual int VerticesCount() const override {
+        return static_cast<int>(matrix.size());
+    }
+    virtual void GetNextVertices(int vertex, std::vector<int>& vertices) const override {
+        if (vertex >= matrix.size())
+            return;
+        for (int j = 1, je = matrix.size(); j != je; ++j) {
+            if (matrix[vertex][j]) {
+                vertices.push_back(matrix[0][j]);
+            }
+        }
+    }
+    virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const override {
+        if (vertex >= matrix.size())
+            return;
+        for (int j = 1, je = matrix.size(); j != je; ++j) {
+            if (matrix[j][vertex]) {
+                vertices.push_back(matrix[j][0]);
+            }
+        }
+    }
+    virtual std::vector<int> GetVertices() const override {
+        std::vector<int> tmp;
+        for (int i = 1, ie = matrix.size(); i != ie; ++i) {
+            for (int j = 1, je = matrix.size(); j != je; ++j) {
+                if (matrix[i][j]) {
+                    tmp.push_back(matrix[i][0]);
+                    break;
+                }
+            }
+        }
+        return tmp;
+    };
+
+    void print() {
+        int max = matrix.size();
+        for (int i = 0, ie = max; i < ie; ++i) {
+            for (int j = 0, je = max; j < je; ++j) {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    ListGraph list;
+    list.AddEdge(1, 2);
+    list.AddEdge(2, 3);
+    list.AddEdge(2, 4);
+    list.AddEdge(3, 4);
+    list.AddEdge(4, 1);
+
+    MatrixGraph matrix(list);
+    matrix.AddEdge(4, 3);
+    matrix.AddEdge(5, 6);
+    matrix.AddEdge(5, 6);
+    matrix.AddEdge(6, 4);
+
+    matrix.print();
+
+    ListGraph lg = matrix;
+
+    std::vector<int> v = lg.GetVertices();
+    for (auto& e : v) {
+        std::cout << e << " ";
+    }
+    v.clear();
+    std::cout << std::endl;
+    lg.GetNextVertices(4, v);
+    for (auto& e : v) {
+        std::cout << e << " ";
+    }
 }
--- Module 31/SmartPointerImplementation/main.cpp
+++ Module 31/SmartPointerImplementation/main.cpp
@@ -14,22 +14,76 @@ private:
 
 class shared_ptr_toy
 {
+public:
+    shared_ptr_toy() = default;
 
+    shared_ptr_toy(Toy* _ptr) {
+        ptr = _ptr;
+        count = new int(1);
+    }
+
+    shared_ptr_toy(std::string name) {
+        ptr = new Toy(name);
+        count = new int(1);
+    }
+
+    shared_ptr_toy(const shared_ptr_toy& oth) {
+        ptr = oth.ptr;
+        count = oth.count;
+        ++(*count);
+    }
+
+    shared_ptr_toy& operator=(const shared_ptr_toy& oth) {
+        if (&oth == this)
+            return *this;
+        if (ptr != nullptr) {
+            --(*count);
+            if (!*count) {
+                delete ptr;
+                delete count;
+            }
+        }
+        ptr = oth.ptr;
+        count = oth.count;
+        ++(*count);
+        return *this;
+    }
+    ~shared_ptr_toy() {
+        --(*count);
+        if (!*count) {
+            delete ptr;
+            delete count;
+        }
+    }
+    Toy* operator->() {
+        return ptr;
+    }
+private:
+    int* count = nullptr;
+    Toy* ptr = nullptr;
 };
 
-shared_ptr_toy make_shared_ptr ()
-{
-
-};
-
-shared_ptr_toy make_shared_ptr()
-{
-
-};
-
+shared_ptr_toy make_shared_ptr(std::string name) {
+    return shared_ptr_toy(name);
+}
+shared_ptr_toy make_shared_ptr(shared_ptr_toy oth) {
+    return shared_ptr_toy(oth);
+}
 
 int main()
 {
+    shared_ptr_toy bone = make_shared_ptr("bone");
+    shared_ptr_toy sphere("sphere");
+    shared_ptr_toy toy(new Toy("toy"));
+    shared_ptr_toy bone_two; // no state
 
+    bone_two = make_shared_ptr(bone); // set state
+
+    std::cout << bone->getName() << " and " << sphere->getName() << std::endl;
+    sphere = bone;
+    std::cout << "Call operator =" << std::endl;
+    std::cout << bone->getName() << " and " << sphere->getName() << std::endl;
+
+    std::cout << "Bone two: " << bone_two->getName() << std::endl;
     return 0;
 }
