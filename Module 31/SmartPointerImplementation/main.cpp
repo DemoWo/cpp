fad0bee2369a67d4d085c6e3898ce6c36436de69
Demon <v1entro@ya.ru>
2023-03-21 10:26:43 +0300

    M31-update

--- Module 31/SmartPointerImplementation/main.cpp
+++ Module 31/SmartPointerImplementation/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+
+class Toy {
+public:
+    Toy() : Toy("unk") {}
+    Toy(std::string _name) : name(_name) {}
+    std::string getName() {
+        return name;
+    }
+private:
+    std::string name;
+};
+
+class shared_ptr_toy
+{
+
+};
+
+shared_ptr_toy make_shared_ptr ()
+{
+
+};
+
+shared_ptr_toy make_shared_ptr()
+{
+
+};
+
 
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
+
     return 0;
 }
