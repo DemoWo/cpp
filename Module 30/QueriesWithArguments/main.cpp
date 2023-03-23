262fdc4882e661b57ef7177f3160e8492d0e7972
Дмитрий <white-wolfs@mail.ru>
2023-03-23 11:22:32 +0300

    M32

--- Module 30/QueriesWithArguments/main.cpp
+++ Module 30/QueriesWithArguments/main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::map<std::string, std::string> args;
     std::string buf1, buf2;
 
-    while (buf1 != "exit")
+    while (true)
     {
         std::cout << "To complete the program, enter \"exit\"!" << std::endl;
         std::cout << "Enter the first value: " << std::endl;
@@ -24,7 +24,7 @@ int main() {
                 data.emplace_back( a.first, a.second );
             }
             args.clear();
-            cpr::Response r = cpr::Post(cpr::Url{ "http://httpbin.org/post" },
+            cpr::Response r = cpr::Post(cpr::Url{ "https://httpbin.org/post" },
                                         cpr::Payload(data.begin(), data.end())
             );
             std::cout << r.text << std::endl;
@@ -43,15 +43,15 @@ int main() {
             }
             data.erase(--data.end());
             args.clear();
-            cpr::Response r = cpr::Get(cpr::Url{ "http://httpbin.org/get" + data });
+            cpr::Response r = cpr::Get(cpr::Url{ "https://httpbin.org/get" + data });
             std::cout << r.text << std::endl;
         }
-        else if (buf1 != "exit")
+        else if (buf1 == "exit") return 0;
+        else
         {
             std::cout << "Enter the second value: " << std::endl;
             std::cin >> buf2;
             args.insert(std::make_pair(buf1, buf2));
         }
     }
-    return 0;
 }
