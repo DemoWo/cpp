e939cd111e3609207ebcfda24c2e39a09e2ce3be
Demon <v1entro@ya.ru>
2023-03-25 16:29:16 +0300

    M32-Task-1

--- Module 32/InformationAboutTheFilmFromTheFilmPortal/main.cpp
+++ Module 32/InformationAboutTheFilmFromTheFilmPortal/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "nlohmann/json.hpp"
+
+struct ACTOR {
+    std::string nameActor = "Name";
+    std::string role;
+};
+
+struct FILM {
+    std::string country;
+    std::string dateCreation;
+    std::string studio = "Test";
+    std::string authorScript;
+    std::string director;
+    std::string producer;
+    ACTOR actor;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    FILM film;
+    std::ofstream file ("record.json");
+    nlohmann::json dir {
+            {"name" , film.studio},
+            {"actor" , film.actor.nameActor}
+    };
+    file << dir;
     return 0;
 }
