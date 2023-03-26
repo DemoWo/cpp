4bf8210952c83ffcd5422479071d3e99df33541e
Demon <v1entro@ya.ru>
2023-03-26 12:21:55 +0300

    M32-Task-1-v-2

--- Module 32/InformationAboutTheFilmFromTheFilmPortal/main.cpp
+++ Module 32/InformationAboutTheFilmFromTheFilmPortal/main.cpp
@@ -5,27 +5,51 @@
 #include "nlohmann/json.hpp"
 
 struct ACTOR {
-    std::string nameActor = "Name";
-    std::string role;
+    // имя актера
+    std::vector <std::string> nameActor = {"Jean Renaud", "Ryoko Hirosue",
+        "Michel Muller", "Yoshi Oida", "Carole Bouquet",
+        "Christian Sinniger"};
+    // исполнение роли
+    std::vector <std::string> role = {"Hubert Fiorentini", "Yumi Yoshimido",
+        "Maurice «Momo»", "Takanawa","Sofia","Le Squale"};
 };
 
 struct FILM {
-    std::string country;
-    std::string dateCreation;
-    std::string studio = "Test";
-    std::string authorScript;
-    std::string director;
-    std::string producer;
-    ACTOR actor;
+    std::string nameFilm = "Wasabi"; // имя киноленты
+    std::string country = "France, Japan"; // страна производства
+    std::string dateCreation = "2001 year"; // дата киноленты
+    std::string studio = "EuropaCorp"; // студия
+    std::string authorScript = "Luc Besson"; //  автор сценария
+    std::string director = "Luc Besson"; // режисер
+    std::string producer = "Gerard Kravchik"; // продюсер
+    ACTOR actor ;
 };
 
 int main() {
     FILM film;
     std::ofstream file ("record.json");
-    nlohmann::json dir {
-            {"name" , film.studio},
-            {"actor" , film.actor.nameActor}
-    };
-    file << dir;
+    if (file.is_open()) {
+        nlohmann::json dir;
+        nlohmann::json actorArr;
+        for (int i = 0; i < film.actor.nameActor.size(); i++) {
+            actorArr[i]["name"] = film.actor.nameActor[i];
+            actorArr[i]["role"] = film.actor.role[i];
+        }
+        file << actorArr;
+
+        dir["name film"] = film.nameFilm;
+        dir["country"] = film.country;
+        dir["date creation"] = film.dateCreation;
+        dir["studio"] = film.studio;
+        dir["author script"] = film.authorScript;
+        dir["director"] = film.director;
+        dir["producer"] = film.producer;
+
+        file << dir;
+        std::cout << "The file was created successfully" << std::endl;
+    } else {
+        std::cout << "File closed!" << std::endl;
+    }
+    file.close();
     return 0;
 }
