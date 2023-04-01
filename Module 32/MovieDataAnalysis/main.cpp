e518669c146dd2c71b6e6813fda0ed7555ea6deb
Demon <v1entro@ya.ru>
2023-04-01 23:26:19 +0300

    M-32-Task-2-v-1

--- Module 32/MovieDataAnalysis/main.cpp
+++ Module 32/MovieDataAnalysis/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    std::ifstream file("cinema_base.json");
+    if (!file.is_open()) {
+        std::cout << "File not found" << std::endl;
+        return 1;
+    }
+    nlohmann::json data;
+    file >> data;
+    file.close();
+
+    std::cout << "Enter actor name or subname" << std::endl;
+    std::string findBuf;
+    std::cin >> findBuf;
+    std::vector<std::string> cinema;
+    std::cout << "Searching.." << std::endl;
+    for (nlohmann::json::iterator it = data.begin(); it != data.end(); ++it) {
+        nlohmann::json c = (*it)["actors"];
+        for (auto& e : c) {
+            if (e["name"].get<std::string>().find(findBuf) != std::string::npos)
+                cinema.push_back(it.key());
+        }
+    }
+    if (cinema.empty())
+        std::cout << "Actor not found" << std::endl;
+    else {
+        std::cout << "Films:" << std::endl;
+        for (auto& e : cinema) {
+            std::cout << e << std::endl;
+        }
+    }
 }
